fe118171c8c20b63be6c0f0a1ba0c5a15844cc88
Caleb <88009118+traitortots@users.noreply.github.com>
2023-04-12 19:16:04 +0000

    test function

--- Scrapy_ArcSpider/Scrapy_ArcSpider/function_app.py
+++ Scrapy_ArcSpider/Scrapy_ArcSpider/function_app.py
@@ -1,7 +1,10 @@
 import logging
+import io
+import sys
 import azure.functions as func
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from azure.storage.blob import BlobServiceClient, BlobUploadOptions
 
 from Scrapy_ArcSpider.spiders import ArcSpider
 
@@ -10,7 +13,30 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function received a request.")
 
     process = CrawlerProcess(get_project_settings())
-    process.crawl(MySpider)
+
+    # Redirect the standard output to a StringIO object
+    old_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+
+    # Start the Scrapy spider
+    process.crawl(ArcSpider)
     process.start()
 
-    return func.HttpResponse("Scrapy spider has started.")
+    # Get the CSV data from the StringIO object and reset the standard output
+    csv_data = sys.stdout.getvalue()
+    sys.stdout = old_stdout
+
+    # Set up the Azure Blob Storage client
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=arcspider921e;AccountKey=JwK2HUcmzJUFHl5PnMpt6wuTIxCI8NdkRpOBfBcY4BqVRHqEEJqt3ADXU97OCwgTR2Q07+MpM65L+AStKiX6ow==;EndpointSuffix=core.windows.net"
+    container_name = "spider-results"
+    blob_name = "result.csv"
+
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+
+    # Upload the CSV data to Azure Blob Storage
+    blob_upload_options = BlobUploadOptions(content_settings=ContentSettings(content_type='text/csv'))
+    blob_client.upload_blob(csv_data, blob_type="BlockBlob", overwrite=True, upload_options=blob_upload_options)
+
+    return func.HttpResponse("Scrapy spider has completed and the CSV data has been uploaded to Azure Blob Storage.")
--- Scrapy_ArcSpider/Scrapy_ArcSpider/settings.py
+++ Scrapy_ArcSpider/Scrapy_ArcSpider/settings.py
@@ -86,3 +86,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_FORMAT = 'csv'
+FEED_URI = 'stdout:'
