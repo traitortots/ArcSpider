f6221f5527b8293212ef6495c0425193de9504e0
Caleb <88009118+traitortots@users.noreply.github.com>
2023-04-13 16:35:51 +0000

    switch to use azure output bindings

--- function_app.py
+++ function_app.py
@@ -4,12 +4,11 @@ import sys
 import azure.functions as func
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from azure.storage.blob import BlobServiceClient, ContentSettings
 from multiprocessing import Process
 
 from Scrapy_ArcSpider.Scrapy_ArcSpider.spiders.ArcSpider import ArcspiderSpider as ArcSpider
 
-def run_spider():
+def run_spider(output_queue):
     settings = get_project_settings()
     settings['FEED_FORMAT'] = 'csv'
     settings['FEED_URI'] = 'output/result.csv'
@@ -17,7 +16,12 @@ def run_spider():
     process.crawl(ArcSpider)
     process.start()
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    with open('output/result.csv', 'r') as file:
+        csv_data = file.read()
+
+    output_queue.put(csv_data)
+
+def main(req: func.HttpRequest, outputblob: func.Out[str]) -> func.HttpResponse:
     logging.info("Python HTTP trigger function received a request.")
 
     # Redirect the standard output to a StringIO object
@@ -25,25 +29,17 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     sys.stdout = io.StringIO()
 
     # Start the Scrapy spider in a separate process
-    spider_process = Process(target=run_spider)
+    from multiprocessing import Queue
+    output_queue = Queue()
+    spider_process = Process(target=run_spider, args=(output_queue,))
     spider_process.start()
     spider_process.join()
 
-    # Get the CSV data from the StringIO object and reset the standard output
-    csv_data = sys.stdout.getvalue()
+    # Get the CSV data from the output queue and reset the standard output
+    csv_data = output_queue.get()
     sys.stdout = old_stdout
 
-    # Set up the Azure Blob Storage client
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=arcspider921e;AccountKey=JwK2HUcmzJUFHl5PnMpt6wuTIxCI8NdkRpOBfBcY4BqVRHqEEJqt3ADXU97OCwgTR2Q07+MpM65L+AStKiX6ow==;EndpointSuffix=core.windows.net"
-    container_name = "spider-results"
-    blob_name = "result.csv"
+    # Write the CSV data to the output blob
+    outputblob.set(csv_data)
 
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
-    blob_client = container_client.get_blob_client(blob_name)
-
-    # Upload the CSV data to Azure Blob Storage
-    blob_upload_options = BlobUploadOptions(content_settings=ContentSettings(content_type='text/csv'))
-    blob_client.upload_blob(json_data, blob_type="BlockBlob", overwrite=True, upload_options=blob_upload_options)
-
-    return func.HttpResponse("Scrapy spider has completed and the JSON data has been uploaded to Azure Blob Storage.")
+    return func.HttpResponse("Scrapy spider has completed and the CSV data has been uploaded to Azure Blob Storage.")
