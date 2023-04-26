7968c28f0647cb963f243c33879f6f4303590c3d
Caleb <88009118+traitortots@users.noreply.github.com>
2023-04-26 00:25:01 -0400

    add request to base url for JSON first

--- get_arcserver_envelopes.py
+++ get_arcserver_envelopes.py
@@ -1,10 +1,12 @@
-import pandas as pd
-import requests
-import geopandas as gpd
 import urllib3
+import pandas as pd
+import geopandas as gpd
+import requests
 import json
-from json.decoder import JSONDecodeError
 from shapely.geometry import Polygon
+from json.decoder import JSONDecodeError
+from pyproj import CRS
+from pyproj.exceptions import CRSError
 
 # Disable SSL warnings (not recommended for production)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -12,17 +14,6 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Read the CSV file into a DataFrame
 
 services = pd.read_csv("Parcel Scrape 04_23 Results.csv")
-
-# Query parameters
-params = {
-    "where": "1=1",
-    "returnGeometry": "true",
-    "returnTrueCurves": "false",
-    "returnIdsOnly": "false",
-    "returnCountOnly": "false",
-    "returnExtentOnly": "true",
-    "f": "geojson",
-}
 
 # Initialize an empty GeoDataFrame with the required columns
 extents = gpd.GeoDataFrame(columns=["name", "full_link", "source", "geometry"])
@@ -32,87 +23,157 @@ successful_requests = 0
 unsuccessful_requests = 0
 
 for index, row in services.iterrows():
-    # Append "/query" to the URL
-    url = row['full_link'] + "/query"
-
-    # Add necessary parameters for the API request
-    payload = {
-        "where": "1=1",
-        "returnGeometry": "true",
-        "returnTrueCurves": "false",
-        "returnIdsOnly": "false",
-        "returnCountOnly": "false",
-        "returnExtentOnly": "true",
-        "f": "geojson",
-    }
-
+    # Make a request to the base URL with the parameter f=pjson
     try:
-        # Make the API request
-        response = requests.get(url, params=payload, verify=False)
+        response = requests.get(row['full_link'], params={"f": "json"}, verify=False)
     except requests.exceptions.RequestException as e:
-        print(f"Connection error for {url} (Layer: {row['name']}): {e}")
+        print(f"Connection error for {row['full_link']} (Layer: {row['name']}): {e}")
         unsuccessful_requests += 1
         continue
 
     if response.status_code == 200:
         try:
-            # Load the GeoJSON response into a Python dictionary
-            geojson_data = json.loads(response.text)
+            data = json.loads(response.text)
 
-            if 'error' in geojson_data:
-                print(f"Error in response for {row['full_link']}: {geojson_data['error']}")
-                unsuccessful_requests += 1
-                continue
+            if "geometryType" in data and data["geometryType"] == "esriGeometryPolygon":
+              
+                wkid = None  # Initialize wkid variable
 
-            # Extract the bounding box coordinates
-            if 'extent' in geojson_data:
-                if 'bbox' in geojson_data['extent']:
-                    bbox = geojson_data['extent']['bbox']
-                elif all(key in geojson_data['extent'] for key in ['xmin', 'ymin', 'xmax', 'ymax']):
-                    xmin = geojson_data['extent']['xmin']
-                    ymin = geojson_data['extent']['ymin']
-                    xmax = geojson_data['extent']['xmax']
-                    ymax = geojson_data['extent']['ymax']
-                    bbox = [xmin, ymin, xmax, ymax]
+                if 'extent' in data:
+                    # Extract the wkid or wkt from the JSON response
+                    spatial_ref = data['extent']['spatialReference']
+                    if 'wkid' in spatial_ref:
+                        wkid = spatial_ref['wkid']
+                    elif 'wkt' in spatial_ref:
+                        wkid = spatial_ref['wkt']
+                    else:
+                        print(f"No wkid or wkt found for {row['full_link']} (Layer: {row['name']}). Skipping.")
+                        unsuccessful_requests += 1
+                        continue
+
+                    if 'bbox' in data['extent']:
+                        bbox = data['extent']['bbox']
+                    elif all(key in data['extent'] for key in ['xmin', 'ymin', 'xmax', 'ymax']):
+                        xmin = data['extent']['xmin']
+                        ymin = data['extent']['ymin']
+                        xmax = data['extent']['xmax']
+                        ymax = data['extent']['ymax']
+                        bbox = [xmin, ymin, xmax, ymax]
+                    else:
+                        print(f"No bounding box found for {row['full_link']}")
+                        unsuccessful_requests += 1
+                        continue
+                # If the extent key is not available, make a request to the 'query' endpoint
                 else:
-                    print(f"No bounding box found for {row['full_link']}")
+                    query_url = row['full_link'] + "/query"
+                    query_payload = {
+                        "where": "1=1",
+                        "geometryType": "esriGeometryPolygon",
+                        "returnGeometry": "true",
+                        "returnTrueCurves": "false",
+                        "returnIdsOnly": "false",
+                        "returnCountOnly": "false",
+                        "returnExtentOnly": "true",
+                        "f": "geojson",
+                    }
+
+                    try:
+                        query_response = requests.get(query_url, params=query_payload, verify=False)
+                    except requests.exceptions.RequestException as e:
+                        print(f"Connection error for {query_url} (Layer: {row['name']}): {e}")
+                        unsuccessful_requests += 1
+                        continue
+
+                    if query_response.status_code == 200:
+                        try:
+                            geojson_data = json.loads(query_response.text)
+
+                            if 'error' in geojson_data:
+                                print(f"Error in response for {row['full_link']}: {geojson_data['error']}")
+                                unsuccessful_requests += 1
+                                continue
+
+                            if 'extent' in geojson_data:
+                                if 'bbox' in geojson_data['extent']:
+                                    bbox = geojson_data['extent']['bbox']
+                                elif all(key in geojson_data['extent'] for key in ['xmin', 'ymin', 'xmax', 'ymax']):
+                                    xmin = geojson_data['extent']['xmin']
+                                    ymin = geojson_data['extent']['ymin']
+                                    xmax = geojson_data['extent']['xmax']
+                                    ymax = geojson_data['extent']['ymax']
+                                    bbox = [xmin, ymin, xmax, ymax]
+                                else:
+                                    print(f"No bounding box found for {row['full_link']}")
+                                    unsuccessful_requests += 1
+                                    continue
+                            else:
+                                print(f"No bounding box found for {row['full_link']}")
+                                unsuccessful_requests += 1
+                                continue
+                        except JSONDecodeError:
+                            unsuccessful_requests += 1
+                            print(f"Error decoding JSON for {query_url} (Layer: {row['name']}, status code: {query_response.status_code})")
+                    else:
+                        print(f"Error for {query_url} (Layer: {row['name']}, status code: {query_response.status_code})")
+                        unsuccessful_requests += 1
+
+                minx, miny, maxx, maxy = bbox
+
+                # Create the bounding box polygon
+                try:
+                    bbox_polygon = Polygon([(minx, miny), (minx, maxy), (maxx, maxy), (maxx, miny), (minx, miny)])
+                except Exception as e:
+                    print(f"Error creating polygon for {row['full_link']} (Layer: {row['name']}): {e}")
                     unsuccessful_requests += 1
                     continue
-            elif 'bbox' in geojson_data:
-                bbox = geojson_data['bbox']
+
+                # Create the original CRS object
+                if wkid is not None:
+                    try:
+                        original_crs = CRS.from_user_input(wkid)
+                    except CRSError as e:
+                        print(f"Error creating CRS for {row['full_link']} (Layer: {row['name']}): {e}")
+                        # You can either skip the current iteration or set a default CRS
+                        continue  # This will skip the current iteration
+                else:
+                    print(f"No wkid found for {row['full_link']} (Layer: {row['name']}). Skipping.")
+                    unsuccessful_requests += 1
+                    continue
+
+                # Reproject the bounding box polygon to the WGS 84 format (EPSG:4326)
+                bbox_gdf = gpd.GeoDataFrame({"geometry": [bbox_polygon]}, crs=original_crs)
+                bbox_gdf_wgs84 = bbox_gdf.to_crs(epsg=4326)
+                bbox_polygon_wgs84 = bbox_gdf_wgs84.geometry.iloc[0]
+
+                # Append the bbox_polygon and the columns from the "services" DataFrame to the "extents" GeoDataFrame
+                new_row = gpd.GeoDataFrame({
+                    "name": [row["name"]],
+                    "full_link": [row["full_link"]],
+                    "source": [row["source"]],
+                    "geometry": [bbox_polygon_wgs84]
+                })
+                extents = pd.concat([extents, new_row], ignore_index=True)
+
+                successful_requests += 1
+                print(f"Successfully requested {row['full_link']} (Layer: {row['name']})")
             else:
-                print(f"No bounding box found for {row['full_link']}")
+                print(f"Layer {row['name']} is not an esriGeometryPolygon")
                 unsuccessful_requests += 1
-                continue
-
-            minx, miny, maxx, maxy = bbox
-
-            # Create the bounding box polygon
-            try:
-                bbox_polygon = Polygon([(minx, miny), (minx, maxy), (maxx, maxy), (maxx, miny), (minx, miny)])
-            except Exception as e:
-                print(f"Error creating polygon for {row['full_link']} (Layer: {row['name']}): {e}")
-                unsuccessful_requests += 1
-                continue
-
-            # Append the bbox_polygon and the columns from the "services" DataFrame to the "extents" GeoDataFrame
-            new_row = gpd.GeoDataFrame({
-                "name": [row["name"]],
-                "full_link": [row["full_link"]],
-                "source": [row["source"]],
-                "geometry": [bbox_polygon]
-            })
-            extents = pd.concat([extents, new_row], ignore_index=True)
-
-            successful_requests += 1
-            print(f"Successfully requested {row['full_link']} (Layer: {row['name']})")
         except JSONDecodeError:
             unsuccessful_requests += 1
-            print(f"Error decoding JSON for {url} (Layer: {row['name']}, status code: {response.status_code})")
+            print(f"Error decoding JSON for {row['full_link']} (Layer: {row['name']}, status code: {response.status_code})")
     else:
-        print(f"Error for {url} (Layer: {row['name']}, status code: {response.status_code})")
+        print(f"Error for {row['full_link']} (Layer: {row['name']}, status code: {response.status_code})")
         unsuccessful_requests += 1
 
-    print(f"Successful requests: {successful_requests}, Unsuccessful requests: {unsuccessful_requests}, Total requests: {successful_requests + unsuccessful_requests}")
+    # At the end of the loop, print the request status
+    print(f"Row {index + 1}:")
+    print(f"  Successful requests: {successful_requests}")
+    print(f"  Unsuccessful requests: {unsuccessful_requests}")
+    print(f"  Total requests: {successful_requests + unsuccessful_requests}")
+
+# Print the total number of successful and unsuccessful requests
+print(f"\nTotal successful requests: {successful_requests}")
+print(f"Total unsuccessful requests: {unsuccessful_requests}")
 
 extents.to_file("extents.geojson", driver="GeoJSON")
